37a784586db2ab5d29b93154d70ae31ea03c0ce3
Jerushaaa <Jerry.gigar@gmail.com>
2023-03-23 19:04:57 -0700

    ES6_promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.allSettled([uploadPhoto(), createUser()])
-    .then((values) => {
-      const data = values[0];
-      const user = values[1];
-      console.log(`${data.body} ${user.firstName} ${user.lastName}`);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
